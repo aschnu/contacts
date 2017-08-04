3117607d1b6d3fff76b54ac16165b185cbfe5765
aschnu <aschnu@o2.pl>
2017-08-04 14:52:19 +0200

    add Contact, ContactControllorer, home.html

---
+++ src/main/java/contacts/contacts/ContactController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contacts.contacts;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author AGRZEGOR
+ */
+@Controller
+@RequestMapping("/")
+public class ContactController {
+
+    private ContactRepository contactRepo;
+
+    @Autowired
+    public ContactController(ContactRepository contactRepo) {
+        this.contactRepo = contactRepo;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Map<String, Object> model) {
+        List<Contact> contacts = contactRepo.findAll();
+        model.put("contacts", contacts);
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submit(Contact contact) {
+        contactRepo.save(contact);
+        return "redirect:/";
+    }
+
+}
